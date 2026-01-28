8d76f397775dabbb7a0e6142e6d01e55a6ba4e28
Ian Garcia <iangar955@gmail.com>
2026-01-28 14:50:41 -0500

    update syntax of html parameters

--- pages/_document.js
+++ pages/_document.js
@@ -7,12 +7,12 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta http-equiv="X-UA-Compatible" content="IE=edge " />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge " />
           <meta name="viewport" content="width=device-width,initial-scale=1.0" />
           <meta name="format-detection" content="telephone=no" />
-          <meta charset="UTF-8" />
+          <meta charSet="UTF-8" />
           <meta name="Copyright" content="PaperStreet Web Design" />
-          <script src="https://kit.fontawesome.com/c6a738eb67.js" crossorigin="anonymous"></script>
+          <script src="https://kit.fontawesome.com/c6a738eb67.js" crossOrigin="anonymous"></script>
         </Head>
         <body>
           <Main />
